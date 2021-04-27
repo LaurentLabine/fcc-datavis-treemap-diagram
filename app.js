8d96e48ce975c54616c8ea93b50997e95260f0ff
Laurent Labine <laurentlabine@gmail.com>
2021-04-27 19:14:05 +0200

    checking in progress
    took from d3js documentation and managed to reproduce transition between different datasets as well as adding colors.

--- app.js
+++ app.js
@@ -1,4 +1,26 @@
-'use strict';
+const datasets = {
+  kickstarter: {
+    name: 'kickstarter',
+    title: 'Kickstarter Pledges',
+    description: 'Top 100 highest funded projects on Kickstarter grouped by category',
+    url: 'https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/kickstarter-funding-data.json'
+  },
+  videogames: {
+    name: 'videogames',
+    title: 'Video Games Sales',
+    description: 'Top 100 best-selling video games grouped by platform',
+    url: 'https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/video-game-sales-data.json'
+  },
+  movies: {
+    name: 'movies',
+    title: 'Movie Sales',
+    description: 'Top 100 highest grossing movies grouped by genre',
+    url: 'https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/movie-data.json'
+  },
+};
+
+const radios = document.getElementsByName('selection');
+var selection = "";
 
 const margin = { top: 40, right: 10, bottom: 10, left: 10 },
   width = 960 - margin.left - margin.right,
@@ -14,39 +36,54 @@ const div = d3.select("body").append("div")
   .style("left", margin.left + "px")
   .style("top", margin.top + "px");
 
-d3.json("flare.json", function (error, data) {
-  if (error) throw error;
+d3.queue()
+  .defer(d3.json, datasets.kickstarter.url)
+  .defer(d3.json, datasets.movies.url)
+  .defer(d3.json, datasets.videogames.url)
+  .await((error, kickstarterData, movieSalesData, videoGameSalesData) => {
+    if (error) throw error;
 
-  const root = d3.hierarchy(data, (d) => d.children)
-    .sum((d) => d.size);
+    selection = 0;
 
-  const tree = treemap(root);
+    const data = [kickstarterData, movieSalesData, videoGameSalesData]
 
-  const node = div.datum(root).selectAll(".node")
-    .data(tree.leaves())
-    .enter().append("div")
-    .attr("class", "node")
-    .style("left", (d) => d.x0 + "px")
-    .style("top", (d) => d.y0 + "px")
-    .style("width", (d) => Math.max(0, d.x1 - d.x0 - 1) + "px")
-    .style("height", (d) => Math.max(0, d.y1 - d.y0 - 1) + "px")
-    .style("background", (d) => color(d.parent.data.name))
-    .text((d) => d.data.name);
+    const colorScheme = ['#e6194B', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911eb4', '#42d4f4', '#f032e6', '#bfef45', '#fabed4', '#469990', '#dcbeff', '#9A6324', '#fffac8', '#800000', '#aaffc3', '#808000', '#ffd8b1'];
 
-  d3.selectAll("input").on("change", function change() {
-    const value = this.value === "count"
-      ? (d) => { return d.size ? 1 : 0; }
-      : (d) => { return d.size; };
+    const colorScale = d3.scaleOrdinal()
+      .domain(data[selection].children.map(d => d.name))
+      .range(colorScheme);
 
-    const newRoot = d3.hierarchy(data, (d) => d.children)
-      .sum(value);
+    var root = d3.hierarchy(data[selection])
+      .sum((d) => d.value)
+      .sort((a, b) => b.value - a.value) // Here the size of each leave is given in the 'value' field in input data
 
-    node.data(treemap(newRoot).leaves())
-      .transition()
-      .duration(1500)
+    var tree = treemap(root);
+
+    var node = div.datum(root).selectAll(".node")
+      .data(tree.leaves())
+      .enter().append("div")
+      .attr("class", "node")
       .style("left", (d) => d.x0 + "px")
       .style("top", (d) => d.y0 + "px")
       .style("width", (d) => Math.max(0, d.x1 - d.x0 - 1) + "px")
       .style("height", (d) => Math.max(0, d.y1 - d.y0 - 1) + "px")
-  });
-});
+      .style("background", (d) => colorScale(d.parent.data.name))
+      .text((d) => d.data.name);
+
+    d3.selectAll("input").on("change", function change(input) {
+      const selection = this.value
+
+      const newRoot = d3.hierarchy(data[selection]).sum((d) => d.value).sort((a, b) => b.value - a.value)
+
+      node.data(treemap(newRoot).leaves())
+        .transition()
+        .duration(1500)
+        .style("left", (d) => d.x0 + "px")
+        .style("top", (d) => d.y0 + "px")
+        .style("width", (d) => Math.max(0, d.x1 - d.x0 - 1) + "px")
+        .style("height", (d) => Math.max(0, d.y1 - d.y0 - 1) + "px")
+        .style("background", (d) => colorScale(d.parent.data.name))
+        .text((d) => d.data.name);
+
+    });
+  })
---
+++ datasets.js
@@ -0,0 +1,20 @@
+const datasets = {
+  kickstarter: {
+    name: 'kickstarter',
+    title: 'Kickstarter Pledges',
+    description: 'Top 100 highest funded projects on Kickstarter grouped by category',
+    url: 'https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/kickstarter-funding-data.json'
+  },
+  videoGames: {
+    name: 'videoGames',
+    title: 'Video Games Sales',
+    description: 'Top 100 best-selling video games grouped by platform',
+    url: 'https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/video-game-sales-data.json'
+  },
+  movies: {
+    name: 'movies',
+    title: 'Movie Sales',
+    description: 'Top 100 highest grossing movies grouped by genre',
+    url: 'https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/movie-data.json'
+  },
+};
